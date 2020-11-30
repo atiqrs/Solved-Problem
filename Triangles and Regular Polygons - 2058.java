3ad0c6d6c77111c15cacd26b46ceaa6a42b3f299
Atiqur Rahman <atiqrs.me@gmail.com>
2020-11-30 16:26:58 +0600

    Create Triangles and Regular Polygons - 2058.java

---
+++ Triangles and Regular Polygons - 2058.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int employeeNumber,workedHours;
+	float amountPerHour;
+
+	employeeNumber= scan.nextInt();
+	workedHours= scan.nextInt();
+	amountPerHour= scan.nextFloat();
+
+	if(input>2 && input<=1000000000)
+		System.out.println(input-2);
+  }
+}
